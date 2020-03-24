1ae612ffdf91dee1fead39001d16eb9dcdc727fe
Beldim <dimonja@inbox.ru>
2020-03-24 14:14:10 +0300

    Working with variables

---
+++ js/test.js
@@ -0,0 +1,29 @@
+﻿/* Работа с переменными */
+/* 1 */
+/* let a = 3;
+console.log (a); */
+/* 2 */
+/* let a = 10;
+let b = 2;
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b); */
+/* 3 */
+/* let c=15;
+let d=2;
+let result=c+d;
+console.log(result); */
+/* 4 */
+/* let a=10;
+let b=2;
+let c=5;
+console.log(a+b+c); */
+/* 5 */
+/* let a=17;
+let b=10;
+let c = a - b;
+let d = 7;
+let result = c + d;
+console.log(result); */
+
